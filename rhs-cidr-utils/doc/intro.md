8d6b633fd06de9be6cbbe290e7d13904c8f35c4b
Bob Stine <bstine@lgscout.com>
2015-07-26 07:46:20 -0400

    Move project from Shadrahk to its own public repo.

---
+++ rhs-cidr-utils/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rhs-cidr-utils
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ rhs-cidr-utils/test/rhs_cidr_utils/core_test.clj
@@ -0,0 +1,26 @@
+(ns rhs-cidr-utils.core-test
+  (:require [clojure.test      :refer :all]
+            [rhs-cidr-utils.core :refer :all]))
+
+
+(deftest bad-octet-tests
+  (testing "poorly formed dotted-octets"
+    (is (thrown? Exception (dotted->bits "abc")))
+    (is (thrown? Exception (dotted->bits "1.2.3")))))
+
+(def legit-octet "10.11.12.13")
+(def legit-int   168496141)
+
+(deftest happy-path-octet-test
+  (testing "convert legit dotted-octet string."
+    (is (= legit-int (dotted->bits legit-octet)))))
+
+(deftest bad-cidr
+  (testing "poorly formed CIDR string"
+    (is (thrown? Exception (cidr->bitmask legit-octet)))))
+
+(def cidr-mask (- legit-int 13))
+
+(deftest happy-cidr-mask
+  (testing "happy path, cidr->bitmask"
+    (is (= cidr-mask (cidr->bitmask (str legit-octet "/24"))))))
