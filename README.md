b3ac54d115858567c789bbf2d0a00a43f3baa437
Bob Stine <bstine@lgscout.com>
2015-07-26 10:18:00 -0400

    Add descriptions of functions to README.md.  Add tests of the examples given in the doc to the unit tests.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CIDR-Utils
 Clojure implementations of utility functions for CIDRs and IP
+
+**dotted-bits**  Given a string of an IP address in dotted-decimal form, return the IP address as a binary long.  
+E.g., ```(dotted-bits "1.2.3.4")``` returns 0x1020304.
+
+**cidr->bitmask** Given the string form of a CIDR, e.g., "10.0.0.0/8", return the net mask as a binary long.
+E.g., ```(cidr->bitmask "10.0.0.0/8")``` returns 0xa000000.
--- rhs-cidr-utils/test/rhs_cidr_utils/core_test.clj
+++ rhs-cidr-utils/test/rhs_cidr_utils/core_test.clj
@@ -13,7 +13,8 @@
 
 (deftest happy-path-octet-test
   (testing "convert legit dotted-octet string."
-    (is (= legit-int (dotted->bits legit-octet)))))
+    (is (= legit-int (dotted->bits legit-octet)))
+    (is (= 0x1020304 (dotted->bits "1.2.3.4")))))
 
 (deftest bad-cidr
   (testing "poorly formed CIDR string"
@@ -25,17 +26,22 @@
 (deftest happy-cidr-mask
   (testing "happy path, cidr->bitmask"
     (is (= cidr-mask  (cidr->bitmask cidr-str)))
-    (is (= 0xcc000000 (cidr->bitmask "204.0.0.0/8")))))   ;; 204 == 0xCC
+    (is (= 0xcc000000 (cidr->bitmask "204.0.0.0/8")))   ;; 204 == 0xCC
+    (is (= 0xa000000  (cidr->bitmask "10.0.0.0/8")))))
 
 (deftest within-mask
   (testing "within-mask, ip in scope"
     (is (ip-within-mask? (cidr->bitmask "10.5.0.0/16") 16 (dotted->bits "10.5.169.250")))
-    (is (ip-within-mask? (cidr->bitmask "204.0.0.0/8")  8 (dotted->bits "204.255.255.255"))))
+    (is (ip-within-mask? (cidr->bitmask "204.0.0.0/8")  8 (dotted->bits "204.255.255.255")))
+    (is (ip-within-mask? 0xa000000 8 0xa0b001f)))
   (testing "ip not within cidr"
     (is (not (ip-within-mask? (cidr->bitmask "10.5.5.0/23")  23 (dotted->bits "10.5.6.250"))))
-    (is (not (ip-within-mask? (cidr->bitmask "204.0.0.0/10") 10 (dotted->bits "204.127.45.2"))))))
+    (is (not (ip-within-mask? (cidr->bitmask "204.0.0.0/10") 10 (dotted->bits "204.127.45.2"))))
+    (is (not (ip-within-mask? 0xa000000 16 0xa0b001f)))))
 
 (deftest cidr-range
   (testing "legit cidrs"
-    (let [cidr-range (get-cidr-range "10.9.0.0/16")]
-      (is (and (= 0xa090000 (cidr-range 0)) (= 0xa09ffff (cidr-range 1)))))))
+    (let [cidr-range (get-cidr-range "10.9.0.0/16")
+          doc-range  (get-cidr-range "127.64.0.0/16")]  ;; this range is in README.md.
+      (is (and (= 0xa090000 (cidr-range 0)) (= 0xa09ffff (cidr-range 1)))
+          (and (= 0x7f400000 (doc-range 0)) (= 0x7f40ffff (doc-range 1)))))))
