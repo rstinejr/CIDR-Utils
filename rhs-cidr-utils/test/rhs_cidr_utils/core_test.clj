f450e1ce522542740274c664b8129580478872bd
Bob Stine <bstine@lgscout.com>
2015-07-26 08:37:25 -0400

    Fix mask-width error that caused an incorrect failure message. Add more tests for ip-with-mask?

--- rhs-cidr-utils/test/rhs_cidr_utils/core_test.clj
+++ rhs-cidr-utils/test/rhs_cidr_utils/core_test.clj
@@ -24,12 +24,13 @@
 
 (deftest happy-cidr-mask
   (testing "happy path, cidr->bitmask"
-    (is (= cidr-mask (cidr->bitmask cidr-str)))))
+    (is (= cidr-mask  (cidr->bitmask cidr-str)))
+    (is (= 0xcc000000 (cidr->bitmask "204.0.0.0/8")))))   ;; 204 == 0xCC
 
 (deftest within-mask
   (testing "within-mask, ip in scope"
-    (is (ip-within-mask? (cidr->bitmask "10.5.0.0/16")  16 (dotted->bits "10.5.169.250"))))
-    (is (ip-within-mask? (cidr->bitmask "204.0.0.0/24") 24 (dotted->bits "204.255.255.255")))
-  )
-
-
+    (is (ip-within-mask? (cidr->bitmask "10.5.0.0/16") 16 (dotted->bits "10.5.169.250")))
+    (is (ip-within-mask? (cidr->bitmask "204.0.0.0/8")  8 (dotted->bits "204.255.255.255"))))
+  (testing "ip not within cidr"
+    (is (not (ip-within-mask? (cidr->bitmask "10.5.5.0/23")  23 (dotted->bits "10.5.6.250"))))
+    (is (not (ip-within-mask? (cidr->bitmask "204.0.0.0/10") 10 (dotted->bits "204.127.45.2"))))))
