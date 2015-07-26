19473039f4e664523b253665035f00219e8aa0c3
Bob Stine <bstine@lgscout.com>
2015-07-26 09:34:38 -0400

    Release 0.1.0.

--- rhs-cidr-utils/project.clj
+++ rhs-cidr-utils/project.clj
@@ -1,6 +1,6 @@
-(defproject rhs-cidr-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject rhs-cidr-utils "0.1.0"
+  :description "Utilities for working with CIDRs"
+  :url "https://github.com/rstinejr/CIDR-Utils"
+  :license {:name "MIT License"
+            :url "https://github.com/rstinejr/CIDR-Utils/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
